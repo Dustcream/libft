bb36ff595d8a5d149dde6be4cf7fdced0e519910
Dustcream <mullerdustin03@gmail.com>
2022-11-07 22:23:17 +0100

    modified:   ft_memcmp.c 	modified:   ft_memcpy.c 	modified:   ft_memmove.c 	modified:   ft_split.c 	modified:   libft.h

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,3 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void    *ft_memcpy(void *dst, const void *src, size_t len)
+{
+    unsigned char   *ptr_dest;
+    unsigned char   *ptr_src;
+
+    if ((!dst && !src) || !len)
+        return (dst);
+    ptr_dest = (unsigned char *)dst;
+    ptr_src = (unsigned char *)src;
+    while (len--)
+        *ptr_dest++ = *ptr_src++;
+    return (dst);
+}
