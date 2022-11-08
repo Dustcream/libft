7c3d89bf49f2258ab9829d515f1409878aa614e1
Dustcream <mullerdustin03@gmail.com>
2022-11-08 20:12:34 +0100

    modified:   ft_split.c 	modified:   ft_strmapi.c 	modified:   ft_strncmp.c 	modified:   ft_strnstr.c 	modified:   ft_strrchr.c 	modified:   ft_strtrim.c 	modified:   ft_substr.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -9,3 +9,51 @@
 /*   Updated: 2022/11/08 17:18:45 by dmuller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_set(const char c, const char *set)
+{
+	int	i;
+
+	i = 0;
+	if (set)
+	{
+		while (set[i])
+		{
+			if (set[i] == c)
+				return (1);
+			i++;
+		}
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*res;
+	int		i;
+	int		j;
+	int		k;
+
+	k = 0;
+	i = 0;
+	if (s1)
+	{
+		j = ft_strlen(s1);
+		while (is_set(s1[i], set) == 1 && s1[i])
+			i++;
+		while (is_set(s1[j - 1], set) == 1 && j > i)
+			j--;
+		res = malloc(sizeof(char) * (j - i + 1));
+		if (!res)
+			return (NULL);
+		if (i > j)
+			return (res);
+		while (i < j)
+			res[k++] = s1[i++];
+		res[k] = '\0';
+		return (res);
+	}
+	return (NULL);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -9,3 +9,32 @@
 /*   Updated: 2022/11/08 17:19:08 by dmuller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	if (start < ft_strlen(s))
+		len = len + start;
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	if (start < len)
+		sub = malloc(sizeof(char) * (len - start + 1));
+	else
+		sub = malloc(sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = start;
+	while (i < len && s[i])
+	{
+		sub[i - start] = s[i];
+		i++;
+	}
+	sub[i - start] = '\0';
+	return (sub);
+}
